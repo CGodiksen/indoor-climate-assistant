4d7fa9032f7bf02d89bd30094f0b497b23dfe999
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-03-07 21:47:56 +0100

    Added the sensor file.
    Added the sensor file and wrote a discription of the file.

---
+++ bm680.py
@@ -0,0 +1,10 @@
+"""
+This file contains the code for extracting data from the bm680 sensor. The file should be run from the
+raspberry pi that is connected to the sensor.
+
+The file can be run in two different modes:
+readable: Outputting the data with supportive notation that helps in understanding the output.
+pure: Outputting only the pure data as to minimize the size of the output.
+
+The file outputs temperature (C), air pressure (hPa), humidity (%RH), gas resistance (Ohms) and air quality (%).
+"""
