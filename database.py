13ba0a468f9f471db86f683c22762c91936b508d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-03-11 17:03:57 +0100

    Created a new file for database stuff.

---
+++ database.py
@@ -0,0 +1,6 @@
+"""
+Database file for creating tables and inserting and querying from the database related to the bme680 sensor. This file
+contains supportive functions that provide the necessary functionality needed for the AQT assistant. This includes
+functions for creating a table that can hold the data and querying functions that support the temperature and air
+quality warnings together with data visualization.
+"""
