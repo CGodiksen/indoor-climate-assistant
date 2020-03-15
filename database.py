e0d3787978b8614d5dfcdf7c8b9dd4b2ee9c0630
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-03-15 17:34:21 +0100

    Added insertion function.
    Added function to get a database connection and inserting a single reading from the sensor into the PostgreSQL database.

--- database.py
+++ database.py
@@ -1,28 +1,50 @@
 """
-Database file for creating tables and inserting and querying from the database related to the bme680 sensor. This file
+Database file for inserting and querying from the database related to the bme680 sensor. This file
 contains supportive functions that provide the necessary functionality needed for the AQT assistant. This includes
-functions for creating a table that can hold the data and querying functions that support the temperature and air
-quality warnings together with data visualization.
+functions for querying that support the temperature and air quality warnings together with data visualization and
+continuous insertion of data.
 """
 import psycopg2
+import sensor
+import time
 
-try:
-    connection = psycopg2.connect(user="pi",
-                                  password="***REMOVED***",
-                                  host="***REMOVED***",
-                                  port="***REMOVED***",
-                                  database="aqtassistant")
+
+def get_database_connection():
+    try:
+        return psycopg2.connect(user="pi",
+                                password="***REMOVED***",
+                                host="***REMOVED***",
+                                port="***REMOVED***",
+                                database="aqtassistant")
+
+    except (Exception, psycopg2.Error) as pg_error:
+        print("Error while working with PostgreSQL" + pg_error)
+
+
+def insert_sensor_data():
+    # Creating a connection to the PostgreSQL database.
+    connection = get_database_connection()
+
+    # Creating a cursor object that can be used for INSERT statements.
     cursor = connection.cursor()
-    pg_select_query = "select * from livingroom"
 
-    cursor.execute(pg_select_query)
-    print(cursor.fetchall())
+    gas_baseline = sensor.burn_in_sensor()
+    # Wrapping the infinite loop in a try-except to support command line keyboard interruption.
+    try:
+        while True:
+            # Getting the current data from the bme680 sensor.
+            data = sensor.get_sensor_data(gas_baseline)
 
-except (Exception, psycopg2.Error) as error:
-    print("Error while working with PostgreSQL" + error)
-finally:
-    # Closing database connection
-    if connection:
+            # Creating the INSERT query that insert the data into the LivingRoom table.
+            pg_insert_query = "insert into livingroom values (%s, %s, %s, %s, %s)"
+
+            # Executing the query while also replacing the placeholders in the query with the actual data.
+            cursor.execute(pg_insert_query, data)
+
+            # Committing the changes to the database, hereby ending the transaction.
+            connection.commit()
+
+            time.sleep(1)
+    except KeyboardInterrupt:
         cursor.close()
         connection.close()
-        print("PostgreSQL connection is closed")
