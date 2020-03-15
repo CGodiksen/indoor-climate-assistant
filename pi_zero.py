89623fef46ca5da88ac89de89dbbe2c6d17800f7
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-03-15 18:05:19 +0100

    Added new file to run from Pi.
    Added a new file that should tie together the current supportive files and provide a single decisive starting point when running the pi connected to the sensor.

---
+++ pi_zero.py
@@ -0,0 +1,30 @@
+"""
+This file should be run from the Raspberry pi zero that is connected to the bme680 sensor. Running this file starts an
+infinite loop that inserts data into the PostgreSQL database.
+"""
+import database
+import sensor
+import time
+
+
+if __name__ == '__main__':
+    # Creating a connection to the PostgreSQL database.
+    connection = database.get_database_connection()
+
+    # Creating a cursor object that can be used for INSERT statements.
+    cursor = connection.cursor()
+
+    gas_baseline = sensor.burn_in_sensor()
+    # Wrapping the infinite loop in a try-except to support command line keyboard interruption.
+    try:
+        while True:
+            # Getting the current data from the bme680 sensor.
+            data = sensor.get_sensor_data(gas_baseline)
+
+            # Inserting the data into the database.
+            database.insert_sensor_data(data, connection, cursor)
+
+            time.sleep(1)
+    except KeyboardInterrupt:
+        cursor.close()
+        connection.close()
