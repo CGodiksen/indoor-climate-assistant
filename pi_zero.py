7708fa69f0afea16e1e2e049fbc2af7d81de144c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-05-20 12:57:36 +0200

    Fixed a bug that caused the gas reading to be wrong due to the sensor cooling down between readings.
    Also changed the GUI slightly.

--- pi_zero.py
+++ pi_zero.py
@@ -12,16 +12,21 @@ if __name__ == '__main__':
     aqtassistant_db = Database()
 
     gas_baseline = sensor.burn_in_sensor()
+
+    counter = 0
     # Wrapping the infinite loop in a try-except to support command line keyboard interruption.
     try:
         while True:
+            counter += 1
+
             # Getting the current data from the bme680 sensor.
             data = sensor.get_sensor_data(gas_baseline)
 
-            # Inserting the data into the database.
-            aqtassistant_db.insert_sensor_data(data)
+            # Every 60 seconds we insert the data into the database.
+            if counter % 60 == 0:
+                aqtassistant_db.insert_sensor_data(data)
 
-            time.sleep(60)
+            time.sleep(1)
     except KeyboardInterrupt:
         # Closing the database connection if execution is halted.
         aqtassistant_db.close()
